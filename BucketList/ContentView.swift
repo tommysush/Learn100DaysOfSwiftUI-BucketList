b2b185fbbe13548fe06e440b219d2227fb42c1ef
Tommy SuSH <tommy.sush@gmail.com>
2021-02-24 09:09:45 +0800

    Initial Commit

---
+++ BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by SuShenghung on 2021/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
